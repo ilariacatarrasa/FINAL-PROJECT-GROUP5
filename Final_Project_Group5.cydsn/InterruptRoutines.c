a21778f90c7bd59e7a2b0249484b74ba8529a0e8
ilaria <ilaria1.milani@mail.polimi.it>
2020-05-23 12:58:24 +0200

    UART MENU added

--- Final_Project_Group5.cydsn/InterruptRoutines.c
+++ Final_Project_Group5.cydsn/InterruptRoutines.c
@@ -33,6 +33,23 @@ CY_ISR(Custom_ISR_RX)
         case '?':
             // show the menù
             // call function show menù
+            /* MENU' on UART */
+    
+            UART_1_PutString("*************************************\r\n");
+            UART_1_PutString("****************MENU'****************\r\n");
+            UART_1_PutString("\r\n");
+            UART_1_PutString("Command      Result                  \r\n");
+            UART_1_PutString("b            Start data acquisition from the accelerometer and storage on the EEPROM memory \r\n");
+            UART_1_PutString("s            Stop data acquisition from the accelerometer and storage on the EEPROM memory \r\n");
+            UART_1_PutString("f            Configuration of the accelerometer full scale range \r\n");
+            UART_1_PutString("p            Configuration of the accelerometer sampling frequency \r\n");
+            UART_1_PutString("v            Start data visualization in the Bridge Control Panel \r\n");
+            UART_1_PutString("u            Stop data visualization in the Bridge Control Panel \r\n");
+            UART_1_PutString("t            Store the temperature data \r\n");
+            UART_1_PutString("\r\n");
+            UART_1_PutString("*************************************\r\n");
+
+
             SelectionFlag=1;
         break;
         // ON command on UART
